3147571829896467442c1f5bd3b9568a1c0428c3
Hirnzwirbler <fortner.steffen@web.de>
2020-09-29 14:21:24 +0200

    Lesson 8

--- StartMenu.gd
+++ StartMenu.gd
@@ -1,6 +1,6 @@
 extends Node
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		get_tree().change_scene("res://World.tscn")
 	if Input.is_action_just_pressed("ui_cancel"):
