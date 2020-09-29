f4145c82cffccb9c6786cda950de544ae29fc9b9
Hirnzwirbler <fortner.steffen@web.de>
2020-09-29 09:50:00 +0200

    Lesson 4

--- Ship.gd
+++ Ship.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+const ExplosionEffect = preload("res://ExplosionEffect.tscn")
 const Bullet = preload("res://Bullet.tscn")
 
 export(int) var SPEED = 100
@@ -22,3 +23,9 @@ func fire_bullet():
 func _on_Ship_area_entered(area):
 	area.queue_free()
 	queue_free()
+
+func _exit_tree():
+	var main = get_tree().current_scene
+	var explosionEffect = ExplosionEffect.instance()
+	main.add_child(explosionEffect)
+	explosionEffect.global_position = global_position
